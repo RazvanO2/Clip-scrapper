7ef201e0942f157f1acaf17f6460d677eb9aae66
Far0 <40454984+Far0@users.noreply.github.com>
2019-08-26 10:28:59 +0300

    update
    Am sters string-urile inutile si le-am pus intr-un array.
    Am optimizat partea a doua a, a testului folosind un loop for.
    Momentan programul este instabil.

--- Night-Scrapper/MainWindow.xaml.vb
+++ Night-Scrapper/MainWindow.xaml.vb
@@ -3,37 +3,31 @@ Imports System.Text
 Imports System.Net
 Class MainWindow
     Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-        Dim fileReader As String
+        Dim fisier As String
         Dim cmd As New Process
-        Dim argument As New ProcessStartInfo("cmd.exe", "/c py -m pip install requests > temp_a.txt && py -m pip install dpath > temp_b.txt && py -m pip install xlwt > temp_c.txt")
-        Dim git_py As String = "https://gist.githubusercontent.com/Far0/78beb7eed6c5f9ca51ab0569f53ee320/raw"
-        Dim git_app As String = "https://gist.githubusercontent.com/Far0/f7539ccf65cfe3b8f396b8bdd98d7cc6/raw"
-        Dim git_upd As String = "https://gist.githubusercontent.com/Far0/6b6c8dc6e614d6416d46f1dc1ff3708f/raw"
-        Dim git_scr As String = "https://gist.githubusercontent.com/Far0/d8cb0af2bed0284b00bcd1634f36b1d6/raw"
-        Dim git_dwn As String = "https://gist.githubusercontent.com/Far0/581356ead6fa2b813ef7557fc95b9641/raw"
-        Dim client As WebClient = New WebClient()
+        Dim linkuri() As String = {"https://gist.githubusercontent.com/Far0/78beb7eed6c5f9ca51ab0569f53ee320/raw", "https://gist.githubusercontent.com/Far0/f7539ccf65cfe3b8f396b8bdd98d7cc6/raw", "https://gist.githubusercontent.com/Far0/6b6c8dc6e614d6416d46f1dc1ff3708f/raw", "https://gist.githubusercontent.com/Far0/d8cb0af2bed0284b00bcd1634f36b1d6/raw", "https://gist.githubusercontent.com/Far0/581356ead6fa2b813ef7557fc95b9641/raw"}
+        'linkuri(0) - versiune script; linkuri(1) - versiune aplicatie; linkuri(2) updater; linkuri(3) scrapper; linkuri(4) downloader
+        Dim versiune() As Double = {}
+        'versiune(0) - aplicatie; versiune(1) python;
+        Dim scripturi() As String = {}
+        'scripturi(0) - updater; scripturi(1) - downloader; scripturi(2) scrapper;
+
+        Dim modul() As String = {"requests", "dpath", "xlwt"}
+        Dim argument As New ProcessStartInfo("cmd.exe", "/c py --version > python_versiune.txt && py -m pip install " + modul(0) + " > " + modul(0) + ".txt && py -m pip install " + modul(1) + " > " + modul(1) + ".txt && py -m pip install " + modul(2) + " > " + modul(2) + ".txt")
+        Dim web As WebClient = New WebClient()
         Button1.Visibility = Visibility.Hidden
-        Dim updaterrn As StreamReader = New StreamReader(client.OpenRead(git_upd), Encoding.UTF8)
-        Dim updater As String = updaterrn.ReadToEnd
-        updaterrn.Close()
-        Dim versn_py As StreamReader = New StreamReader(client.OpenRead(git_py))
-        Dim vers_py As Double = versn_py.ReadToEnd
-        versn_py.Close()
-        Dim versn_app As StreamReader = New StreamReader(client.OpenRead(git_app))
-        Dim vers As Double = versn_app.ReadToEnd
-        versn_app.Close()
-        Dim dowwn As StreamReader = New StreamReader(client.OpenRead(git_dwn))
-        Dim down As String = dowwn.ReadToEnd
-        dowwn.Close()
-        Dim sccr As StreamReader = New StreamReader(client.OpenRead(git_scr))
-        Dim scrap As String = sccr.ReadToEnd
-        sccr.Close()
+        For i = 0 To linkuri.Length - 1 Step 1
+            Dim browser As StreamReader = New StreamReader(web.OpenRead(linkuri(i)))
+            Dim valori(i) As String = browser.ReadToEnd
+            If linkuri.Length - 1 = i Then
+                browser.Close()
+            End If
+        Next
+
         Label3.Content = "Versiune aplicație: " & My.Settings.verslocal & Environment.NewLine & "Versiune script-uri: " & My.Settings.verslocal_py
         If My.Settings.testul_1 = "0" Then
-            Process.Start("cmd", "/c py --version > t.txt")
-            Threading.Thread.Sleep(1000)
-            fileReader = My.Computer.FileSystem.ReadAllText("t.txt")
-            If Not (fileReader = "") Then
+            fisier = My.Computer.FileSystem.ReadAllText("python_versiune.txt")
+            If Not (fisier = "") Then
                 RichTextBox1.AppendText(Environment.NewLine + "[STAGE 1] Python este instalat.")
                 My.Settings.testul_1 = "1"
             Else
@@ -44,6 +38,12 @@ Class MainWindow
         Else
             RichTextBox1.AppendText(Environment.NewLine + "[STAGE 1] Python este deja instalat.")
         End If
+
+
+
+
+
+
         If My.Settings.verslocal >= vers Then
             If My.Settings.verslocal > vers Then
                 RichTextBox1.AppendText(Environment.NewLine + "[UPDATER] Ai o versiune BETA de Night Scrapper.")
@@ -54,8 +54,8 @@ Class MainWindow
         Else
             MsgBox("Apasă ok pentru a descărca ultima versiune de Night Scrapper :)", vbExclamation, "Nu ai ultima versiune de Night Scrapper")
             If System.IO.File.Exists("updater.py") Then
-                fileReader = My.Computer.FileSystem.ReadAllText("updater.py")
-                If Not (updater = fileReader) Then
+                fisier = My.Computer.FileSystem.ReadAllText("updater.py")
+                If Not (updater = fisier) Then
                     My.Computer.FileSystem.DeleteFile("updater.py")
                     File.WriteAllText("updater.py", updater)
                     Process.Start("cmd", "/c py updater.py")
@@ -68,50 +68,50 @@ Class MainWindow
                 Me.Close()
             End If
         End If
-        If My.Settings.testul_2 = "0" Then
-            cmd.StartInfo = argument
-            cmd.Start()
-            cmd.WaitForExit()
-            fileReader = My.Computer.FileSystem.ReadAllText("temp_a.txt")
-            If fileReader.StartsWith("Requirement") Then
-                RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Requests este instalat deja.")
-            Else
-                RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Modulul Requests s-a instalat.")
-            End If
-            fileReader = My.Computer.FileSystem.ReadAllText("temp_b.txt")
-            If fileReader.StartsWith("Requirement") Then
-                RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Dpath este instalat deja.")
-            Else
-                RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Modulul Dpath s-a instalat.")
-            End If
-            fileReader = My.Computer.FileSystem.ReadAllText("temp_c.txt")
-            If fileReader.StartsWith("Requirement") Then
-                RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Xlwt este instalat deja.")
-            Else
-                RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Modulul Xlwt s-a instalat.")
-            End If
-            My.Settings.testul_2 = 1
-            Try
-                My.Computer.FileSystem.DeleteFile("temp_a.txt")
-                My.Computer.FileSystem.DeleteFile("temp_b.txt")
-                My.Computer.FileSystem.DeleteFile("temp_c.txt")
-                My.Computer.FileSystem.DeleteFile("t.txt")
-            Catch ex As Exception
-                RichTextBox1.AppendText(Environment.NewLine + "[INFO] Nu au fost găsite fișiere temporare.")
-            End Try
-        Else
-            RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Toate modulele necesare sunt instalate deja.")
+        ' My.Settings.testul_2 = "0"
+        If True Then
+            For i = 0 To modul.Length - 1 Step 1
+                fisier = My.Computer.FileSystem.ReadAllText(modul(i) & ".txt")
+                If fisier.StartsWith("Requirement") Then
+                    RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Modulul " & modul(i) & " este instalat deja.")
+                Else
+                    RichTextBox1.AppendText(Environment.NewLine + "[STAGE 2] Modulul " & modul(i) & " s-a instalat.")
+                End If
+                Try
+                    My.Computer.FileSystem.DeleteFile(modul(i) & ".txt")
+                Catch ex As Exception
+                    RichTextBox1.AppendText(Environment.NewLine + "[INFO] Nu a fost log file-ul pentru " & modul(i) & ".txt")
+
+                End Try
+            Next
+
         End If
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         If System.IO.File.Exists("scrapper.py") And System.IO.File.Exists("downloader.py") Then
             RichTextBox1.AppendText(Environment.NewLine + "[STAGE 3] Am găsit cele 2 scripturi python.")
-            fileReader = My.Computer.FileSystem.ReadAllText("scrapper.py")
-            If Not (scrap = fileReader) Then
+            fisier = My.Computer.FileSystem.ReadAllText("scrapper.py")
+            If Not (scrap = fisier) Then
                 My.Computer.FileSystem.DeleteFile("scrapper.py")
                 File.WriteAllText("scrapper.py", scrap)
                 RichTextBox1.AppendText(Environment.NewLine + "[STAGE 3] Scrapper.py s-a updatat.")
             End If
-            fileReader = My.Computer.FileSystem.ReadAllText("Downloader.py")
-            If Not (down = fileReader) Then
+            fisier = My.Computer.FileSystem.ReadAllText("Downloader.py")
+            If Not (down = fisier) Then
                 My.Computer.FileSystem.DeleteFile("Downloader.py")
                 File.WriteAllText("downloader.py", down)
                 RichTextBox1.AppendText(Environment.NewLine + "[STAGE 3] Downloader.py s-a updatat.")
@@ -127,8 +127,8 @@ Class MainWindow
         RichTextBox1.AppendText(Environment.NewLine + "[FINISHED] Am terminat.")
 
         Dim win1 As Window1 = New Window1()
-        win1.Show()
-        Me.Close()
+        '     win1.Show()
+        '   Me.Close()
     End Sub
     Private Sub Button1_Click(sender As Object, e As RoutedEventArgs) Handles Button1.Click
         My.Settings.testul_1 = 0
